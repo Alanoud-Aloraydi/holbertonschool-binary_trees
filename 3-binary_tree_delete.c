f487d383bd1ec5f44cbc19079e5a42b4a6accd4f
Ghaida <alsabtighaida@gmail.com>
2026-04-28 17:12:27 +0000

    Task 3: delete a tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes a tree
+ * @tree: Pointer to the tree to be deleted
+ *
+ * Return: nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL) /* if tree is null, do nothing */
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
